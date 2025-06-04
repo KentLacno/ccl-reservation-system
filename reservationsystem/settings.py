513afcebe21f9ca0317567c7fe84513eaa6868b4
Kent Harvey Lacno <kentharveylacno@Kents-Mac-mini.local>
2025-06-04 15:33:07 +0800

    Remove __cache__ and gitignore settings.py until we find a better way to store .env

--- reservationsystem/settings.py
+++ reservationsystem/settings.py
@@ -138,7 +138,8 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
